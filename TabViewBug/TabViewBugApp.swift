bcba85c24d9728a42b783bf7c3d20edfefa024b0
werb <werbhelius@gmail.com>
2020-10-25 13:06:48 +0800

    Initial Commit

---
+++ TabViewBug/TabViewBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewBugApp.swift
+//  TabViewBug
+//
+//  Created by wanbo on 2020/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TabViewBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
